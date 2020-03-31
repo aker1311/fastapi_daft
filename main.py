6fee91157c62e6236ebfeaec051cb4b09a78c26c
Arek S <arek1311@gmail.com>
2020-03-31 14:13:59 +0200

    Added base model

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
 app = FastAPI()
+
+class HelloResp(BaseModel):
+    msg: str
 
 @app.get('/')
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
-@app.get("/hello/{name}")
+@app.get("/hello/{name}", response_model=HelloResp)
 def read_item(name: str):
-    return f"Hello {name}"
+    return HelloResp(msg=f"Hello {name}")
+
+
+#@app.get("/{METHOD}")
+#def read_method(METHOD: str):
+#    return {"method": f'{METHOD}'}
