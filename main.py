19d43afa4bf4f9197039cfcfb564b9497a74f9ab
Arek S <arek1311@gmail.com>
2020-04-02 16:24:21 +0200

    Homework 1

--- main.py
+++ main.py
@@ -3,17 +3,29 @@ from fastapi import FastAPI, Request
 from pydantic import BaseModel
 
 app = FastAPI()
+app.counter = 0
 
 class HelloResp(BaseModel):
     msg: str
+
+class Patient(BaseModel):
+    name: str
+    surname: str
+
+#-------------------------------------------------- 
 
 @app.get('/')
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
+
+#---------- Homework 1 Problem 1
+
 @app.get('/hello/{name}', response_model=HelloResp)
 def read_item(name: str):
     return HelloResp(msg=f"Hello {name}")
+
+#---------- Homework 1 Problem 2
 
 @app.get('/method')
 def read_method(request: Request):
@@ -29,3 +41,10 @@ def read_method(request: Request):
 def read_method(request: Request):
     used_method = request.method
     return {'method': used_method}
+
+#---------- Homework 1 Problem 3
+
+@app.post('/patient')
+def get_patient(patient: Patient):
+    app.counter +=1
+    return {'id': str(app.counter), patient} 
