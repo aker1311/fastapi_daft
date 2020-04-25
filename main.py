bbebd03bab9ce06e5e5d37df911d13c468174a32
Arek S <arek1311@gmail.com>
2020-04-25 21:29:25 +0200

    Homework 2

--- main.py
+++ main.py
@@ -4,26 +4,34 @@ from pydantic import BaseModel
 
 app = FastAPI()
 app.counter = 0
-global patients
+#global patients
 patients = []
 
 
 class HelloResp(BaseModel):
     msg: str
 
+class Patient(BaseModel):
+    name: str
+    surename: str
+
+
+class PatientID(BaseModel):
+    id: int
+    patient: Patient 
+
+
 @app.get('/hello/{name}', response_model=HelloResp)
 def read_item(name: str):
     return HelloResp(msg=f"Hello {name}")
-
-# --------------------------------------------------
-
-# ---------- Homework 1 Problem 
 
 @app.get('/')
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
-# ---------- Homework 1 Problem 2
+@app.get('/welcome')
+def welcome():
+    return {"message": "Welcome user"}
 
 @app.get('/method')
 def read_method(request: Request):
@@ -40,31 +48,17 @@ def read_method(request: Request):
     return {'method': request.method}
 
 
-# ---------- Homework 1 Problem 3
-
-class Patient(BaseModel):
-    name: str
-    surename: str
-
-
-class PatientID(BaseModel):
-    id: int
-    patient: Patient 
-
-
 @app.post('/patient', response_model=PatientID)
 def add_patient(request: Patient):
-
+    global patients
     p = PatientID(id = app.counter, patient = request)
     app.counter+=1
     patients.append(p)
     return p
 
-# ---------- Homework 1 Problem 4
-
 @app.get('/patient/{pk}')
 def read_patient(pk: int):
-
+    global patients
     if pk not in [i.id for i in patients]:
        return JSONResponse(status_code = 204, content = {}) 
     return patients[pk].patient
