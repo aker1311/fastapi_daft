795fcb6dfafc40174d04be96fe32aa4526f646c8
Arek S <arek1311@gmail.com>
2020-04-27 00:08:55 +0200

    H2

--- test_main.py
+++ test_main.py
@@ -21,8 +21,4 @@ def test_get_patient():
     for i in range(1,3):
         response = client.post('/patient', json={'name': 'Jan', 'surname': 'Kowalski'})
         assert response.status_code == 200
-        assert response.json() == {'id': i+1, 'patient': {'name': 'Jan', 'surname': 'Kowalski'}}
-
-#def test_receive_something():
-#    response = client.post("/dej", json={'first_key': 'some_value','sec_key': 'other value'})
-#    assert response.json() == {'received': {'first_key': 'some_value', 'sec_key': 'other value'},'constant_data': 'python jest super'}
+        assert response.json() == {'id': i, 'patient': {'name': 'Jan', 'surname': 'Kowalski'}}
