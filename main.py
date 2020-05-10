3dbcee060825c41b6070c4c144671b3622114a62
Arek S <arek1311@gmail.com>
2020-05-10 20:01:24 +0200

    H2

--- main.py
+++ main.py
@@ -190,7 +190,7 @@ async def album(album_id: int):
 @app.put('/customers/{customer_id}')
 async def edit_customer(customer: Customer, customer_id: int):
     app.db_connection.row_factory = sqlite3.Row
-    find = app.db_connection.execute(f"SELECT customerid FROM customers WHERE customerid = ?", (customer_id, )).fetchone()
+    find = app.db_connection.execute(f"SELECT * FROM customers WHERE customerid = ?", (customer_id, )).fetchone()
     if find is not None:
         return find
     raise HTTPException(status_code=404, detail={"error": "Customer not found"})
